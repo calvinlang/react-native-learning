de475c37cd8cc446102175a4471151e2464a7216
Calvin Lang <langcalvin@gmail.com>
2016-11-30 16:20:38 +0800

    Created LoginForm Component

---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React { Component } from 'react';
+import ReactNative from 'react-native';
+import { Card } from './common';
+
+class LoginForm extends Component {
+	render() {
+		return(
+			<Card>
+				
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;
