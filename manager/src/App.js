69da48226331f6a417d7b1c31b78f1536c1df2d5
Calvin Lang <langcalvin@gmail.com>
2016-12-01 10:32:12 +0800

    Wired up password field on Login

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
 		return (
 			<Provider store={createStore(reducers)}>
 				<View>
+					<Header />
 					<LoginForm />
 				</View>
 			</Provider>
