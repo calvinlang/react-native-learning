8af7d1ffe113ded8b2365048464dec51af3c16a2
Calvin Lang <langcalvin@gmail.com>
2016-12-01 11:16:02 +0800

    Wired up the login component to firebase

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
@@ -22,7 +23,12 @@ class App extends Component {
 
 	render() {
 		return (
-			<Provider store={createStore(reducers)}>
+			<Provider store={createStore(
+				reducers, 
+				{}, 
+				applyMiddleware(ReduxThunk)
+				)}
+			>
 				<View>
 					<Header />
 					<LoginForm />
